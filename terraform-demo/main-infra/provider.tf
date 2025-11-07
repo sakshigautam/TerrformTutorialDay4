2521446bc791c066f5f353081c9db8347c514b69
Sakshi Gautam <gautamsakshi551@gmail.com>
2025-11-07 18:58:07 +0530

    Add Azure and GCP backend configurations
    Added backend configurations for Azure and GCP to provider.tf.

--- terraform-demo/main-infra/provider.tf
+++ terraform-demo/main-infra/provider.tf
@@ -8,6 +8,7 @@ terraform {
     }
   }
 
+# AWS
   backend "s3" {
     bucket         = "my-terraform-remote-state-demo-bucket"  # from backend-setup output
     key            = "demo/terraform.tfstate"
@@ -16,6 +17,34 @@ terraform {
     encrypt        = true
   }
 }
+
+# Azure
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-rg"
+    storage_account_name = "terraformstorageacct"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
+
+# GCP
+
+terraform {
+  backend "gcs" {
+    bucket  = "terraform-state-demo"
+    prefix  = "terraform/state"
+  }
+}
+
+  backend "gcs" {
+    bucket  = "terraform-state-demo"
+    prefix  = "terraform/state"
+  }
+}
+
+
+
 
 provider "aws" {
   region = "us-east-1"
